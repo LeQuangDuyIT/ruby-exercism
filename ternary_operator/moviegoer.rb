e66d74ac64b0788dd22df214a835a42c1999dbf1
LeQuangDuyIT <lqduycp@gmail.com>
2024-07-29 22:44:50 +0700

    ternary operator - moviegoer

---
+++ ternary_operator/moviegoer.rb
@@ -0,0 +1,22 @@
+class NotMovieClubMemberError < RuntimeError
+end
+
+class Moviegoer
+
+def initialize(age, member: false)
+  @age = age
+  @member = member
+end
+
+def ticket_price
+  @age >= 60 ? 10 : 15
+end
+
+def watch_scary_movie?
+  @age >= 18
+end
+
+def claim_free_popcorn!
+   @member ? "🍿" : (raise NotMovieClubMemberError, "Only movie club members can claim free popcorn.")
+end
+end
